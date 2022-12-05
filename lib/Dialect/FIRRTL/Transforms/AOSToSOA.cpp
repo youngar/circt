2fa0d3212abdbcb1252ed3e7c05610c4764288dc
Robert Young <robert.young@shopify.com>
2022-12-05 10:32:19 -0500

    WIP: Converting the ports now

--- lib/Dialect/FIRRTL/Transforms/AOSToSOA.cpp
+++ lib/Dialect/FIRRTL/Transforms/AOSToSOA.cpp
@@ -18,41 +18,294 @@
 #include "circt/Dialect/FIRRTL/FIRRTLVisitors.h"
 #include "circt/Dialect/FIRRTL/Passes.h"
 #include "circt/Support/FieldRef.h"
-#include "llvm/ADT/MapVector.h"
+// #include "llvm/ADT/MapVector.h"
 #include "llvm/ADT/STLExtras.h"
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/ADT/ArrayRef.h"
+
+#define DEBUG_TYPE "firrtl-aos-to-soa"
 
 using namespace circt;
 using namespace firrtl;
 
-namespace {
+// namespace {
 
-struct TypeAccumulation {
+// struct TypeAccumulation {};
 
-};
+// bool convertFVectorType(FVectorType vectorType) {
+//   auto elementType = vectorType.getElementType();
+//   if (auto bundleElementType = dyn_cast<BundleType>(elementType)) {
+//     return false; // convertBundleType(bundleElementType);
+//   }
+//   return false;
+// }
 
-bool convertFVectorType(FVectorType vectorType) {
-  auto elementType = vectorType.getElementType();
-  if (auto bundleElementType = dyn_cast<BundleType>(elementType)) {
-    return false; // convertBundleType(bundleElementType);
-  }
-  return false;
-}
-
-bool convertType(Type type) {
-  if (auto vectorType = dyn_cast<FVectorType>(type))
-    return convertFVectorType(vectorType);
-  return false;
-}
+// bool convertType(Type type) {
+//   if (auto vectorType = dyn_cast<FVectorType>(type))
+//     return convertFVectorType(vectorType);
+//   return false;
+// }
 
 // bool convertPort(port) {
 
 // }
 
-bool convertFModulePorts(FModuleOp moduleOp) {
+// bool convertPort() {}
+
+// bool convertUsers(Value value) {
+//   for (auto op : value.use_begin()) {
+//   }
+// }
+
+//===----------------------------------------------------------------------===//
+// Type Rewriter
+//===----------------------------------------------------------------------===//
+
+namespace {
+class TypeRewriteTable {
+public:
+  TypeRewriteTable();
+
+  Type rewrite(Type);
+
+private:
+  // True if the given type is a vector of struct.
+  bool needsRewrite(Type) const;
+
+  Type buildRewrite(Type) const;
+};
+}
+
+TypeRewriteTable::TypeRewriteTable() {}
+
+Type TypeRewriteTable::rewrite(Type t) {
+  // if the type doesn't require any rewrite, just return it.
+  if (!needsRewrite(t)) {
+    return nullptr;
+  }
+
+  return t;
+}
+
+bool TypeRewriteTable::needsRewrite(Type t) const {
+  // auto vt = t.dyn_cast<VectorType>();
+  // if (vt == nullptr) {
+  //   return false;
+  // }
+
+  // auto et = vt.getElementType();
+  // while (et.isa<VectorType>()) {
+  //   if (et.isa<BundleType>())
+  //     return true;
+  //   if (!et.isa<VectorType>())
+  //     return false;
+
+  //   auto vt = et.dyn_cast<VectorType>();
+  //   et = vt.getElementType();
+  // }
+
   return false;
 }
 
+namespace {
+class TypeConverterX {
+public:
+  static FIRRTLBaseType convert(MLIRContext *context, FIRRTLBaseType type);
+  static TypeAttr convert(MLIRContext *context, TypeAttr attr);
+  static Type convert(MLIRContext *context, Type type);
+
+private:
+  class State {
+  public:
+    std::vector<unsigned> dimensions;
+  };
+
+  class Stash {
+  public:
+    Stash(State &state) : state(state), dimensionality(state.dimensions.size()) {}
+
+    ~Stash() {
+      state.dimensions.resize(dimensionality);
+    }
+
+  private:
+    State &state;
+    unsigned dimensionality;
+  };
+
+  static FIRRTLBaseType convert(MLIRContext *context, State &state, FIRRTLBaseType type);
+};
+}
+
+TypeAttr TypeConverterX::convert(MLIRContext *context, TypeAttr attr) {
+  return TypeAttr::get(convert(context, attr.getValue()));
+}
+
+Type TypeConverterX::convert(MLIRContext *context, Type type) {
+  if (auto ft = type.dyn_cast<FIRRTLBaseType>()) {
+    return convert(context, ft).dyn_cast<Type>();
+  }
+  return type;
+}
+
+FIRRTLBaseType TypeConverterX::convert(MLIRContext *context, FIRRTLBaseType type) {
+  State state;
+  return convert(context, state, type);
+}
+
+FIRRTLBaseType TypeConverterX::convert(MLIRContext *context, State &state, FIRRTLBaseType type) {
+
+  // Vector Types
+
+  if (auto vectorType = type.dyn_cast<FVectorType>(); vectorType) {
+    state.dimensions.push_back(vectorType.getNumElements());
+    return convert(context, state, vectorType.getElementType());
+  }
+
+  // Bundle Types
+
+  if (auto bundleType = type.dyn_cast<BundleType>(); bundleType) {
+    SmallVector<BundleType::BundleElement> elements;
+    for (auto element : bundleType.getElements()) {
+      Stash guard(state);
+      elements.push_back(BundleType::BundleElement(element.name, element.isFlip, convert(context, state, element.type)));
+    }
+
+    return BundleType::get(elements, context);
+  }
+
+  // Ground Types
+
+  for (auto size : llvm::reverse(state.dimensions)) {
+    type = FVectorType::get(type, size);
+  }
+  return type;
+}
+
+//===----------------------------------------------------------------------===//
+// Visitor
+//===----------------------------------------------------------------------===//
+
+namespace {
+class LiftBundlesVisitor
+    : public FIRRTLVisitor<LiftBundlesVisitor, LogicalResult> {
+public:
+  explicit LiftBundlesVisitor(MLIRContext *context);
+
+  using FIRRTLVisitor<LiftBundlesVisitor, LogicalResult>::visitDecl;
+  using FIRRTLVisitor<LiftBundlesVisitor, LogicalResult>::visitExpr;
+  using FIRRTLVisitor<LiftBundlesVisitor, LogicalResult>::visitStmt;
+
+  LogicalResult visit(FModuleOp op);
+
+private:
+  Type convertType(Type type);
+
+  Attribute convertTypeAttr(Attribute attr);
+
+  DenseMap<FieldRef, SmallVector<FieldRef>> mappings;
+  MLIRContext *context;
+  TypeRewriteTable typeRewriteTable;
+};
 } // end anonymous namespace
+
+LiftBundlesVisitor::LiftBundlesVisitor(MLIRContext *context)
+    : mappings(), context(context), typeRewriteTable() {}
+
+// LogicalResult LiftBundlesVisitor::visit(SubfieldOp op) { return success(); }
+
+// LogicalResult LiftBundlesVisitor::visit(SubindexOp op) { return success(); }
+
+// LogicalResult LiftBundlesVisitor::visitGeneric(Operation *op) {
+//   // for (auto operand & op->getOperands()) {
+//   //   convertO
+//   // }
+//   return success();
+// }
+
+// LogicalResult LiftBundlesVisitor::visit(SubaccessOp op) {
+//   return success();
+// }
+
+// LogicalResult LiftBundlesVisitor::visitConnect(ConnectOp op) {
+//   // auto helper = [*](SmallVector outerTypes) {
+//   //   auto aType = a.getType();
+//   //   auto bType = b.getType();
+//   //   if (a.type == vector) {
+//   //     outerTypes.push_back
+//   //   }
+//   // }
+
+//   return success();
+// }
+
+LogicalResult LiftBundlesVisitor::visit(FModuleOp op) {
+  auto numPorts = op.getNumPorts();
+
+  // SmallVector<Direction> newDirections;
+  // SmallVector<Attribute> newNames, newTypes, newAnnos, newSyms;
+  SmallVector<Attribute> newPortTypes;
+  newPortTypes.resize(numPorts);
+
+  // newDirections.reserve(numPorts);
+  // newNames.reserve(numPorts);
+  // newTypes.reserve(numPorts);
+  // newAnnos.reserve(numPorts);
+  // newSyms.reserve(numPorts);
+
+  // SmallVector<TypeAttr> newPortTypeAttrs;
+  // newPortTypeAttrs.resize(e);
+
+  for (size_t i = 0; i < numPorts; ++i) {
+    newPortTypes[i] = TypeConverterX::convert(context, op.getPortTypeAttr(i)).cast<Attribute>();
+  }
+
+  // op->setAttr("portDirections",
+  //             direction::packAttribute(op.getContext(), newDirections));
+  // op->setAttr("portNames", ArrayAttr::get(op.getContext(), newNames));
+  op->setAttr("portTypes", ArrayAttr::get(op.getContext(), newPortTypes));
+  // op->setAttr("portAnnotations", ArrayAttr::get(op.getContext(), newAnnos));
+  // op.setPortSymbols(newSyms);
+
+  auto body = op.getBodyBlock();
+  for (unsigned i = 0, e = body->getNumArguments(); i < e; ++i) {
+    auto argument = body->getArgument(i);
+    argument.setType(newPortTypes[i].dyn_cast<TypeAttr>().getValue());
+  }
+  return success();
+}
+
+Type LiftBundlesVisitor::convertType(Type type) {
+  return typeRewriteTable.rewrite(type);
+}
+
+
+//===----------------------------------------------------------------------===//
+// Module Op Conversion
+//===----------------------------------------------------------------------===//
+
+// bool convertPort() { auto type = op.getType(); }
+
+// bool convertPortsofFModuleOp(FModuleOp moduleOp) {
+//   auto changed = false;
+//   auto ports = op.getPorts();
+//   auto i = size_t(0);
+//   auto e = ports.size();
+//   for (; i < e; ++i) {
+//     changed = convertPort(ports, index) || changed;
+//   }
+
+//   return changed;
+// }
+
+// bool convertFModuleOp(FModuleOp op) {
+//   bool changed = false;
+//   changed = convertFModulePorts() || changed;
+//   changed = convertFModuleBody() || changed;
+//   return changed;
+// }
+
+// } // end anonymous namespace
 
 //===----------------------------------------------------------------------===//
 // Pass Infrastructure
@@ -65,12 +318,15 @@ class AOSToSOAPass : public AOSToSOABase<AOSToSOAPass> {
 } // end anonymous namespace
 
 void AOSToSOAPass::runOnOperation() {
+  // auto visitor = LiftBundlesVisitor(&getContext());
+  LiftBundlesVisitor visitor(&getContext());
+  visitor.visit(getOperation());
   markAllAnalysesPreserved();
-  // ModuleVisitor visitor;
-  // if (!visitor.run(getOperation()))
-  //   markAllAnalysesPreserved();
-  // if (failed(visitor.checkInitialization()))
-  //   signalPassFailure();
+  // auto changed = convertFModuleOp(getOperation());
+  auto changed = false;
+  if (!changed) {
+    markAllAnalysesPreserved();
+  }
 }
 
 std::unique_ptr<mlir::Pass> circt::firrtl::createAOSToSOAPass() {
--- test/Dialect/FIRRTL/aos-to-soa.mlir
+++ test/Dialect/FIRRTL/aos-to-soa.mlir
@@ -1,13 +1,66 @@
-// RUN: circt-opt --pass-pipeline='builtin.module(firrtl.circuit(firrtl-aos-to-soa))' | FileCheck %s
+// RUN: circt-opt --pass-pipeline='builtin.module(firrtl.circuit(firrtl.module(firrtl-aos-to-soa)))' %s | FileCheck %s
 
-firrtl.circuit "AOSToSOATestCircuit" {
-  // CHECK-LABEL: @Test1
-  firrtl.module @Test1() {
+// test cases
+// - base type
+// - struct
+// - vector of base type
+// - vector of struct
+// - vector of vector of base type
+// - vector of vector of struct
+// - struct of vector of struct
+// - 
 
+// - wire
+// - port
+// - register?
+
+// have to use the value, too
+// 
+firrtl.circuit "Test" {
+  firrtl.module @Test() {}
+
+  // vector<ground> -> vector<ground> (no change)
+  // CHECK-LABEL:  @VG
+  // CHECK-SAME:   (in %port: !firrtl.vector<uint<8>, 4>)
+  firrtl.module @VG(in %port: !firrtl.vector<uint<8>, 4>) {
+    // no change
   }
 
-  // CHECK-LABEL: @Test2
-  firrtl.module @Test2() {
+  // bundle<ground> -> bundle<ground> (no change)
+  // CHECK-LABEL:  @BG
+  // CHECK-SAME:   (in %port: !firrtl.bundle<a: uint<8>>)
+  firrtl.module @BG(in %port: !firrtl.bundle<a: uint<8>>) {
+    // no change
+  }
+
+  // vector<bundle> -> bundle<vector>
+  // CHECK-LABEL:  @VB
+  // CHECK-SAME:   (in %port: !firrtl.bundle<field: vector<uint<1>, 4>>)
+  firrtl.module @VB(in %port: !firrtl.vector<bundle<a: uint<8>>, 4>) {
+    // port[i].field => port.field[i]
+    // %bundle = firrtl.subindex %port[0] : !firrtl.vector<bundle<a: uint<8>>, 4> -> bundle<a: uint<8>>
+    // %field  = firrlt.subfield %bundle(0) : !firrtl.bundle<a: uint<8>> -> uint<8>
+  }
+
+  // vector<bundle<bundle>> -> bundle<bundle<vector>>
+  // CHECK-LABEL:   @VBB
+  // CHECK-SAME:    (in %port: !firrtl.bundle<nested: bundle<field: vector<uint<1>, 4>>>)
+  firrtl.module @VBB(in %port: !firrtl.vector<bundle<nested: bundle<field: uint<1>>>, 4>) {
+    // port[i].nested.field => port.nested.field[i]
+  }
+
+  // vector<vector<bundle>> -> bundle<vector<vector>
+  // CHECK-LABEL:   @VVB
+  // CHECK-SAME:    (in %port: !firrtl.bundle<field: vector<vector<uint<1>, 6>, 4>>)
+  firrtl.module @VVB(in %port: !firrtl.vector<vector<bundle<field: uint<1>>, 6>, 4>) {
+    // port[i][j].field => port.field[i][j]
+  }
+
+  // vector<bundle<vector<bundle>> -> bundle<bundle<vector<vector>>>
+  // CHECK-LABEL:    @VBVB
+  // CHECK-SAME:     (in %port: !firrtl.bundle<field_a: bundle<field_b: vector<vector<uint<1>, 4>, 8>>>)
+  firrtl.module @VBVB(in %port: !firrtl.vector<bundle<field_a: vector<bundle<field_b: uint<1>>, 4>>, 8>) {
 
   }
+  // bundle<vector<bundle>> -> bundle<bundle<vector>>
 }
